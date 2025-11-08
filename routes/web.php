3e0d577b9a90e19da63386679a6932d14b0a0739
TridevSharma1 <tridevx8@gmail.com>
2025-11-08 21:05:33 +0530

    update 4

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/product-list', [ProductController::class, 'product_list'])->name('product.list');
-// Route::post('/product-store', [ProductController::class, 'product_store']);
 Route::get('/product-store-form', [ProductController::class, 'product_store_form'])->name('product.store.form');
 Route::post('/product-store', [ProductController::class, 'product_store'])->name('product.store');
 Route::delete('/product/{id}', [ProductController::class, 'SoftDeleteproduct'])->name('product.destroy');
 Route::get('/product/{id}/edit', [ProductController::class, 'productedit'])->name('product.edit');
 Route::put('/product/{id}', [ProductController::class, 'productupdate'])->name('product.update');
 Route::get('/product-home', [ProductController::class, 'producthome'])->name('product.home');
-
+Route::get('/product/{id}', [ProductController::class, 'productView'])->name('product.view');
 
 
 // There are 4 parts in URL
@@ -22,3 +21,6 @@ Route::get('/product-home', [ProductController::class, 'producthome'])->name('pr
 // 2 :- controller Name
 // 3 :- Third One Is function Name
 // 4 :- Name which is used to Redirect or Mantion in form tag in any form
+
+
+// Route::post('/product-store', [ProductController::class, 'product_store']);
